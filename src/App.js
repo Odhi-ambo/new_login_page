ca39cfa7bfc00477c7a571afe3317b8f70949515
Odhi-ambo <michaelderrick605@gmail.com>
2024-03-28 22:03:46 +0300

    working directory with forms for the UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+      </div>
+      
+  );
+}
+
+export default App;
