98958dff19e44aee8835aa04c4013eb45f7e5070
Odhi-ambo <michaelderrick605@gmail.com>
2024-03-29 17:48:21 +0300

    Refactored LoginForm component to utilize CSS styling from LoginForm.css for enhanced styling consistency and maintainability.

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './LoginForm.css';
-import { FaUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
+
 
 const LoginForm = () => {
   return (
@@ -9,26 +9,28 @@ const LoginForm = () => {
         <div className='wrapper'>
             <form action="">
                 <h1>Login</h1>
-                <div className='input-box'>
-                    <input type ='text' placeholder='Username' required/>
+                <div className="input-box">
+                    <input type ="text" placeholder='Username' required/>
                     <FaUser className='icon'/>
                 </div>
-                <div className='input-box'>
-                    <input type ='password' placeholder='Password' required/>
+                <div className="input-box">
+                    <input type ="password" placeholder='Password' required/>
                     <FaLock className='icon' />
                 </div>
-                <div className='remember-forgot'>
-                    <label><input type='checkbox'/>Remember me</label>
+                <div className="remember-forgot">
+                    <label><input type="checkbox"/>Remember me</label>
                     <a href="#">Forgot password</a>
                 </div>
-                <button type='submit'>Login</button>
-                <div className='register-link'>
+                <button type="submit">Login</button>
+                <div className="register-link">
                     <p>Don't have an account? <a href="#">Register</a></p>
                 </div>
+                
             </form>
+            </div>
         </div>
       
-    </div>
+    
   )
 }
 
